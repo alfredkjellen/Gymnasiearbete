531f1c7003a0745ae47192a96ee6709318de9cf4
alfredkjellen <81372499+alfredkjellen@users.noreply.github.com>
2024-01-20 17:23:00 +0100

    Create competition_script.py

---
+++ competition_script.py
@@ -0,0 +1,15 @@
+class Competition:
+    
+    def __init__(self, name):
+        self.name  = name
+        
+
+    date = 0
+    h18result = []
+    h20result = []
+    h21result = []
+    d18result = []
+    d20result = []
+    d21result = []
+
+    
